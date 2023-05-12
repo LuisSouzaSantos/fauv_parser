ec1d09696545f5b53beaf8da337e942f28dfa410
Luis Gustavo de Souza Santos <luis.gustavo.ss@hotmail.com>
2023-05-12 00:16:44 -0300

    FAUV-84 - Configuring startup

--- src/main/java/com/fauv/parser/ParserApplication.java
+++ src/main/java/com/fauv/parser/ParserApplication.java
@@ -3,10 +3,15 @@ package com.fauv.parser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fauv.parser.pattern.configuration.PatternConfiguration;
+
 @SpringBootApplication
 public class ParserApplication {
 
 	public static void main(String[] args) {
+		//First Arg is the path of Regex files;
+		PatternConfiguration.PATH = args[0];
+		
 		SpringApplication.run(ParserApplication.class, args);
 	}
 
--- src/main/java/com/fauv/parser/pattern/configuration/PatternConfiguration.java
+++ src/main/java/com/fauv/parser/pattern/configuration/PatternConfiguration.java
@@ -19,10 +19,11 @@ public class PatternConfiguration {
 	
     private final Logger logger = LoggerFactory.getLogger(PatternConfiguration.class);
 
-	private static final String BLOCK_PATTERN_CONFIGURATION_FILE_PATH = "/usr/local/fauv/parserRegex/blockPattern.txt";
-	private static final String INVALID_PATTERN_CONFIGURATION_FILE_PATH = "/usr/local/fauv/parserRegex/ignorePattern.txt";
-	private static final String LINE_PATTERN_CONFIGURATION_FILE_PATH = "/usr/local/fauv/parserRegex/linePattern.txt"; 
-	private static final String	LINE_INFORMATION_PATTERN_CONFIGURATION_FILE_PATH = "/usr/local/fauv/parserRegex/contentPattern.txt"; 
+    public static String PATH = "";
+	private static final String BLOCK_PATTERN_CONFIGURATION_FILE_PATH = "/blockPattern.txt";
+	private static final String INVALID_PATTERN_CONFIGURATION_FILE_PATH = "/ignorePattern.txt";
+	private static final String LINE_PATTERN_CONFIGURATION_FILE_PATH = "/linePattern.txt"; 
+	private static final String	LINE_INFORMATION_PATTERN_CONFIGURATION_FILE_PATH = "/contentPattern.txt"; 
 	
 	private List<BlockPattern> blockList = new ArrayList<BlockPattern>();
 	private List<AdditionalInformation> additionalInformationList = new ArrayList<AdditionalInformation>();
@@ -53,84 +54,100 @@ public class PatternConfiguration {
 	}
 	
 	private void loadIgnoreContent() throws FileNotFoundException, IOException {
-		String blockContent = Utils.readFile(new File(INVALID_PATTERN_CONFIGURATION_FILE_PATH));
+		String blockContent = Utils.readFile(new File(PATH+INVALID_PATTERN_CONFIGURATION_FILE_PATH));
 
 		String[] lines = blockContent.split("\r\n");
 		
 		for (String line : lines) {
-			String[] keyAndValue = line.split(":=");
-			
-			ignoreInformation.add(keyAndValue[1]);
+			try {
+				String[] keyAndValue = line.split(":=");
+				
+				ignoreInformation.add(keyAndValue[1]);
+			} catch (Exception e) {
+				logger.info("Load Ignore Content function not recognize line: "+line);
+			}
 		}
-		
 	}
 	
 	private void loadBlockContent() throws FileNotFoundException, IOException {
-		String blockContent = Utils.readFile(new File(BLOCK_PATTERN_CONFIGURATION_FILE_PATH));
+		String blockContent = Utils.readFile(new File(PATH+BLOCK_PATTERN_CONFIGURATION_FILE_PATH));
 		
 		String[] lines = blockContent.split("\r\n");
 		
 		for (int i = 0; i < lines.length; i = i + 3) {
-			String[] firstInformation = lines[i].split(":=");
-			String[] secondInformation = lines[i+1].split(":=");
-			String[] thirdInformation = lines[i+2].split(":=");
-		
-			BlockPattern blockPattern = new BlockPattern();
-			blockPattern.setInit(firstInformation[1]);
-			blockPattern.setEnd(secondInformation[1]);
-			blockPattern.setType(BlockPatternType.valueOf(thirdInformation[1]));
+			try {
+				String[] firstInformation = lines[i].split(":=");
+				String[] secondInformation = lines[i+1].split(":=");
+				String[] thirdInformation = lines[i+2].split(":=");
 			
-			this.blockList.add(blockPattern);
+				BlockPattern blockPattern = new BlockPattern();
+				blockPattern.setInit(firstInformation[1]);
+				blockPattern.setEnd(secondInformation[1]);
+				blockPattern.setType(BlockPatternType.valueOf(thirdInformation[1]));
+				
+				this.blockList.add(blockPattern);
+			}catch (Exception e) {
+				logger.info("Load Block Content function not recognize line: "+lines[i]);
+			}
+
 		}
 		
-		logger.debug("Number of BLOCK CONTENT loaded is: "+this.blockList.size());
+		logger.info("Number of BLOCK CONTENT loaded is: "+this.blockList.size());
 	}
 	
 	private void loadLineContent() throws FileNotFoundException, IOException {
-		String lineContent = Utils.readFile(new File(LINE_PATTERN_CONFIGURATION_FILE_PATH));
+		String lineContent = Utils.readFile(new File(PATH+LINE_PATTERN_CONFIGURATION_FILE_PATH));
 		
 		String[] lines = lineContent.split("\r\n");
 		
 		for (String line : lines) {
-			String[] information = line.split(":=");
-			
-			LinePattern linePattern = new LinePattern();
-			linePattern.setName(information[0]);
-			linePattern.setPattern(information[1]);
-			
-			LinePatternType type = LinePatternType.whichIs(information[0]);
-			
-			linePattern.setType(type);
-			linePattern.setAdditionalInformation(getByLinePatternName(information[0]));
-			
-			this.linePatternList.put(linePattern.getName(), linePattern);
+			try {
+				String[] information = line.split(":=");
+				
+				LinePattern linePattern = new LinePattern();
+				linePattern.setName(information[0]);
+				linePattern.setPattern(information[1]);
+				
+				LinePatternType type = LinePatternType.whichIs(information[0]);
+				
+				linePattern.setType(type);
+				linePattern.setAdditionalInformation(getByLinePatternName(information[0]));
+				
+				this.linePatternList.put(linePattern.getName(), linePattern);
+			}catch (Exception e) {
+				logger.info("Load Line Content function not recognize line: "+line);
+			}
 		}
 
-		logger.debug("Number of LINE CONTENT loaded is: "+this.linePatternList.size());
+		logger.info("Number of LINE CONTENT loaded is: "+this.linePatternList.size());
 	}
 	
 	private void lineInformation() throws FileNotFoundException, IOException {
-		String lineInformation = Utils.readFile(new File(LINE_INFORMATION_PATTERN_CONFIGURATION_FILE_PATH));
+		String lineInformation = Utils.readFile(new File(PATH+LINE_INFORMATION_PATTERN_CONFIGURATION_FILE_PATH));
 
 		String[] lines = lineInformation.split("\r\n");
 		
-		for (String line : lines) { 
-			String[] information = line.split(":=");
-			String[] indexs = information[1].split(",");
-			
-			AdditionalInformation additionalInformation = new AdditionalInformation();
-			additionalInformation.setName(information[0]);
-			
-			for (int i = 0; i < indexs.length; i++) {
-				String value = indexs[i];
-				additionalInformation.getValues().add(i, Integer.parseInt(value));
+		for (String line : lines) {
+			try {
+				String[] information = line.split(":=");
+				String[] indexs = information[1].split(",");
 				
+				AdditionalInformation additionalInformation = new AdditionalInformation();
+				additionalInformation.setName(information[0]);
+				
+				for (int i = 0; i < indexs.length; i++) {
+					String value = indexs[i];
+					additionalInformation.getValues().add(i, Integer.parseInt(value));
+					
+				}
+				
+				this.additionalInformationList.add(additionalInformation);
+			}catch (Exception e) {
+				logger.info("Load Line Information function not recognize line: "+line);
 			}
-			
-			this.additionalInformationList.add(additionalInformation);
 		}
 		
-		logger.debug("Number of ADDITIONAL INFORMATION loaded is: "+this.additionalInformationList.size());
+		logger.info("Number of ADDITIONAL INFORMATION loaded is: "+this.additionalInformationList.size());
 	}
 	
 	private AdditionalInformation getByLinePatternName(String linePatternName) {
